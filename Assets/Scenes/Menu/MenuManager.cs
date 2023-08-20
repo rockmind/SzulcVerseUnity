aaa4df8e36baa07104a282c01363262b6a3a8883
stillimproving <xkasienka@gmail.com>
2023-08-20 12:59:48 +0200

    + UI

---
+++ Assets/Scenes/Menu/MenuManager.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Toggle = UnityEngine.UI.Toggle;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private Toggle toggleFrames;
+    public static bool Frames = false;
+    [SerializeField] private TMP_InputField inputRecursionLimit;
+    public static int RecursionLimit = 3;
+    [SerializeField] private Toggle toggleTorus;
+    public static bool Torus;
+    [SerializeField] private Toggle toggleKleinBottle;
+    public static bool KleinBottle;
+    [SerializeField] private Toggle toggleLandscape1;
+    [SerializeField] private Toggle toggleLandscape2;
+    [SerializeField] private Toggle toggleLandscape3;
+
+    private void Start()
+    {
+        toggleFrames.isOn = Frames;
+        inputRecursionLimit.text = RecursionLimit.ToString();
+        toggleTorus.isOn = Torus;
+        toggleKleinBottle.isOn = KleinBottle;
+
+    }
+
+    public void StartVisualisation()
+    {
+        RecursionLimit = Convert.ToInt32(inputRecursionLimit.text);
+        Frames = toggleFrames.isOn;
+        Torus = toggleTorus.isOn;
+        KleinBottle = toggleKleinBottle.isOn;
+        
+        if (toggleTorus.isOn)
+        {
+            SceneManager.LoadScene("TorusScene");
+        }
+
+        if (toggleKleinBottle.isOn)
+        {
+            SceneManager.LoadScene("KleinScene");
+        }
+    }
+
+    public void ExitVisualisation()
+    {
+        Application.Quit();
+    }
+}
